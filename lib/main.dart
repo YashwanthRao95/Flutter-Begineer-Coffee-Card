a7616505909447f6f8a9450e2141340abc26bd90
Yashwanth Rao Gujja <yashwanthgujja95@gmail.com>
2024-09-18 17:30:21 -0500

    Flutter beginner course - Coffee Card final commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:coffee_card/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home()
+  ));
+}
+
+class SandBox extends StatelessWidget {
+  const SandBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            height: 100,
+            color: Colors.red,
+            child: const Text('One'),
+          ),
+          Container(
+            height: 200,
+            color: Colors.green,
+            child: const Text('Two'),
+          ),
+          Container(
+            height: 300,
+            color: Colors.blue,
+            child: const Text('Three'),
+          )
+        ],
+      )
+    );
+  }
+}
